4a1ac993ba06b4e46f132889e4fe2ae64d250107
Bryan Romero <109822721+Bryan-Romero@users.noreply.github.com>
2023-07-07 15:26:48 -0700

    Update setings.js

--- src/config/setings.js
+++ src/config/setings.js
@@ -1,9 +1,10 @@
-require('dotenv/config');
+require("dotenv/config");
 
 module.exports = {
-    MYSQL_URI: process.env.MYSQL_URI,
-    MYSQL_USER: process.env.MYSQL_USER,
-    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
-    MYSQL_DB: process.env.MYSQL_DB,
-    SECRET: process.env.SECRET_TOKEN
-}
+  MYSQL_URI: process.env.MYSQL_URI,
+  MYSQL_USER: process.env.MYSQL_USER,
+  MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+  MYSQL_DB: process.env.MYSQL_DB,
+  SECRET: process.env.SECRET_TOKEN,
+  PORT: process.env.PORT,
+};
